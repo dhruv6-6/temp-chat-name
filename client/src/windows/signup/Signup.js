0dc8ed4e61fdbc7d9d72e6dc33b7aa703b1230fb
dhruv6-6 <dhurv6.sharma6@gmail.com>
2023-06-21 19:00:58 +0530

    signup screen v1 ready

--- client/src/windows/signup/Signup.js
+++ client/src/windows/signup/Signup.js
@@ -7,10 +7,17 @@ const Signup = ()=>{
         <div className="full_screen_box">
             <div className='login_area'>
                 <div className='welcome'>
-                    <p1 className="welcome_message">Welcome Back</p1>
+                    <p className="welcome_message">Welcome Back</p>
+                    <p className="bottom_welcome_message"><p className="new_here">New here?</p><p className='create_account'>Create an account</p></p>
                 </div>
-                <div className='username'></div>
-                <div className='password'></div>
+                <div className='username'>
+                    <p className='username_writting'>Username</p>
+                    <input className='username_input'></input>
+                </div>
+                <div className='password'>
+                    <p className='password_writting'>Password</p>
+                    <input className='password_input'></input>
+                </div>
             </div>
         </div>
     )
