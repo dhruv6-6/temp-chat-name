7f86fea566b50e102bf5f021edb4bc6b5c39c294
dhruv6-6 <dhurv6.sharma6@gmail.com>
2023-06-21 17:17:22 +0530

    signup screen first commit

--- client/src/windows/signup/Signup.js
+++ client/src/windows/signup/Signup.js
@@ -5,6 +5,13 @@ import './Signup.css'
 const Signup = ()=>{
     return(
         <div className="full_screen_box">
+            <div className='login_area'>
+                <div className='welcome'>
+                    <p1 className="welcome_message">Welcome Back</p1>
+                </div>
+                <div className='username'></div>
+                <div className='password'></div>
+            </div>
         </div>
     )
 }
