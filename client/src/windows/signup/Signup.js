438d3b6d09ec1f07e8613a1ea8685d5e8abdf554
dhruv6-6 <dhurv6.sharma6@gmail.com>
2023-06-21 21:17:49 +0530

    signup css fix

--- client/src/windows/signup/Signup.js
+++ client/src/windows/signup/Signup.js
@@ -4,18 +4,18 @@ import './Signup.css'
 
 const Signup = ()=>{
     return(
-        <div className="full_screen_box">
-            <div className='login_area'>
-                <div className='username'>
-                    <p className='username_writting'>Username</p>
-                    <input className='username_input'></input>
+        <div className="full_screen_box_2">
+            <div className='login_area_2'>
+                <div className='username_2'>
+                    <p className='username_writting_2'>Username</p>
+                    <input className='username_input_2'></input>
                 </div>
-                <div className='password'>
-                    <p className='password_writting'>Password</p>
-                    <input className='password_input'></input>
+                <div className='password_2'>
+                    <p className='password_writting_2'>Password</p>
+                    <input className='password_input_2'></input>
                 </div>
-                <div className='button_area'>
-                    <button className='sign_up_button'>Sign Up</button>
+                <div className='button_area_2'>
+                    <button className='sign_up_button_2'>Sign Up</button>
                 </div>
             </div>
         </div>
