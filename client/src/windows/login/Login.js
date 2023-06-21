ba1cfb639ddcd0f9190679dbaaeb92c57d8d4632
dhruv6-6 <dhurv6.sharma6@gmail.com>
2023-06-21 21:06:17 +0530

    login signup swap

--- client/src/windows/login/Login.js
+++ client/src/windows/login/Login.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React from 'react';
+import './Login.css'
+
 
 const Login = ()=>{
     return(
-        <div>
-            <h1>Login</h1>
+        <div className="full_screen_box">
+            <div className='login_area'>
+                <div className='welcome'>
+                    <p className="welcome_message">Welcome Back</p>
+                    <div className="bottom_welcome_message">
+                        <p className="new_here">New here?</p>
+                        <p className='create_account'>Create an account</p>
+                    </div>
+                </div>
+                <div className='username'>
+                    <p className='username_writting'>Username</p>
+                    <input className='username_input'></input>
+                </div>
+                <div className='password'>
+                    <p className='password_writting'>Password</p>
+                    <input className='password_input'></input>
+                </div>
+            </div>
         </div>
     )
 }
--- client/src/windows/signup/Signup.js
+++ client/src/windows/signup/Signup.js
@@ -6,10 +6,6 @@ const Signup = ()=>{
     return(
         <div className="full_screen_box">
             <div className='login_area'>
-                <div className='welcome'>
-                    <p className="welcome_message">Welcome Back</p>
-                    <p className="bottom_welcome_message"><p className="new_here">New here?</p><p className='create_account'>Create an account</p></p>
-                </div>
                 <div className='username'>
                     <p className='username_writting'>Username</p>
                     <input className='username_input'></input>
@@ -17,6 +13,9 @@ const Signup = ()=>{
                 <div className='password'>
                     <p className='password_writting'>Password</p>
                     <input className='password_input'></input>
+                </div>
+                <div className='button_area'>
+                    <button className='sign_up_button'>Sign Up</button>
                 </div>
             </div>
         </div>
