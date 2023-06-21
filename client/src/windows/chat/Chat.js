cb21882f8f1dd85385b9d54383b514abd25ee670
dhruv6-6 <dhurv6.sharma6@gmail.com>
2023-06-21 21:07:39 +0530

    chat main push

--- client/src/windows/chat/Chat.js
+++ client/src/windows/chat/Chat.js
@@ -1,9 +1,32 @@
 import React from "react";
+import './Chat.css';
+
+import dots from './images/3dots.png';
+import adduser from './images/adduser.png'
+import call from './images/call.png'
+import videocall from './images/videocall.png'
+import logout from './images/logout.png'
+import callhistory from './images/callhistory.png'
+import settings from './images/settings.png'
+import users from './images/users.png'
 
 const Chat = ()=>{
     return(
-        <div>
-            <h1>Chat</h1>
+        <div className="main_body">
+            <div className="sidebar">
+                
+            </div>
+            <div className="user_names_area">
+                <div className="search_user_area">
+                    <input placeholder="Search" className="search_box"></input>
+                    <img src = {dots} className="dots"></img>
+                </div>
+                <div className="user_names"></div>
+            </div>
+            <div className="chat_area">
+                <div className="chatter_info"></div>
+                <div className="chats"></div>
+            </div>
         </div>
     )
 }
